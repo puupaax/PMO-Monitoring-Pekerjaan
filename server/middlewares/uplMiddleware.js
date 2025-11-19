a4b4e75f90622b88072404f5d008250a672e7735
Muhammad Ihsan <mihsan2903@gmail.com>
2025-11-19 23:04:50 +0700

    hide uplmiddleware

--- server/middlewares/uplMiddleware.js
+++ server/middlewares/uplMiddleware.js
@@ -1,22 +1,22 @@
-import multer from "multer";
-import path from "path";
-import fs from "fs";
+// import multer from "multer";
+// import path from "path";
+// import fs from "fs";
 
-const uploadDir = "./uploads";
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir);
-}
+// const uploadDir = "./uploads";
+// if (!fs.existsSync(uploadDir)) {
+//     fs.mkdirSync(uploadDir);
+// }
 
-// Konfigurasi penyimpanan
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, uploadDir);
-    },
-    filename: (req, file, cb) => {
-        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-        cb(null, uniqueSuffix + path.extname(file.originalname));
-    },
-});
+// // Konfigurasi penyimpanan
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, uploadDir);
+//     },
+//     filename: (req, file, cb) => {
+//         const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+//         cb(null, uniqueSuffix + path.extname(file.originalname));
+//     },
+// });
 
-const upload = multer({ storage });
-export default upload;
+// const upload = multer({ storage });
+// export default upload;
